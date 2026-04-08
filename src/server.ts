f7dbc78b27cc73bb80ad09034faee2c84d6e20dd
Wiktor <wiktor-wosiek@wp.pl>
2026-04-08 09:10:22 +0200

    naprawiona baza danch

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,28 @@
-// Ten plik zajmuje się utworzeniem serwera http
-
 import app from './app';
-import {connectDB, createCollections, seedDatabase} from "./db";
+import { connectDB, createCollections, seedDatabase } from "./db";
 
-// użyj portu podanego w `.env`, a jeżeli go nie ma, to użyj portu 3000
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
-    await connectDB()
-    // TODO – createCollections() i seedDatabase()
+    try {
+        await connectDB();
 
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+        await createCollections();
+
+        await seedDatabase();
+
+        app.listen(PORT, () => {
+            console.log(`Serwer Muminkopedii śmiga na porcie: ${PORT}`);
+        });
+
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            console.error(`Krytyczny błąd podczas startu serwera: ${err.message}`);
+        } else {
+            console.error("Wystąpił nieznany błąd podczas startu serwera.");
+        }
+        process.exit(1);
+    }
 }
 
-startServer()
-    .catch((err: unknown)=> {
-        console.log(`Błąd podczas uruchamiania serwera:\n${err}`)
-        process.exit(1)
-    })
+startServer();
