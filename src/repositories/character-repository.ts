52a3638b38047b3e996358702c18a7efe291e605
ignacy11 <ignacy.banet@cyfroweszkoly.pl>
2026-04-16 00:02:05 +0200

    implement POST method for /characters

--- src/repositories/character-repository.ts
+++ src/repositories/character-repository.ts
@@ -1,10 +1,21 @@
 import {CharacterModel} from "../models/character-model";
 import {Character} from "../types/Character";
 
+// GET
 export async function getAllCharacters(): Promise<Character[]> {
     return CharacterModel.find()
 }
 
 export async function getCharacterByID(id: string): Promise<Character | null> {
     return CharacterModel.findById(id)
+}
+
+export async function getCharacterByName(name: string): Promise<Character | null> {
+    return CharacterModel.findOne({name})
+}
+
+// POST
+export async function createCharacter({name, description, species, isHibernating}: Character): Promise<Character> {
+    const newCharacter = new CharacterModel({name, description, species, isHibernating})
+    return await newCharacter.save()
 }
