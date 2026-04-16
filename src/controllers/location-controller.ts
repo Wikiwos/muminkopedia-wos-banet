7e34e4963bb58c01cf51f717779ac675e77d9568
ignacy11 <ignacy.banet@cyfroweszkoly.pl>
2026-04-16 21:56:00 +0200

    implement DELETE method for /locations/:id

--- src/controllers/location-controller.ts
+++ src/controllers/location-controller.ts
@@ -1,5 +1,7 @@
 import {Request, Response} from "express"
 import {fetchLocations} from "../services/location-service";
+import {deleteLocation as deleteLocationService} from "../services/location-service";
+import {LocationParams} from "../types/LocationParams";
 
 // GET
 export async function getLocations(req: Request, res: Response): Promise<void> {
@@ -10,5 +12,18 @@ export async function getLocations(req: Request, res: Response): Promise<void> {
     } catch (err) {
         const errorMessage = err instanceof Error ? err.message : "Nieznany błąd"
         res.status(400).json({error: `Nie udało się załadować miejsc: ${errorMessage}`})
+    }
+}
+
+// DELETE
+export async function deleteLocation(req: Request<LocationParams>, res: Response): Promise<void> {
+    try {
+        const {id} = req.params
+
+        const result = await deleteLocationService(id)
+        res.status(200).json({mesage: `Pomyślnie usunięto lokalizację o id ${id}`})
+    } catch (err) {
+        const errorMessage = err instanceof Error ? err.message : "Nieznany błąd"
+        res.status(400).json({error: `Nie udało się usunąć lokalizacji: ${errorMessage}`})
     }
 }
--- src/services/location-service.ts
+++ src/services/location-service.ts
@@ -1,6 +1,15 @@
 import {Location} from "../types/Location";
-import {getAllLocations} from "../repositories/location-repository";
+import {deleteLocationByID, findAllLocations, getOneLocationByID} from "../repositories/location-repository";
+import {getLocations} from "../controllers/location-controller";
 
 export async function fetchLocations(): Promise<Location[]> {
-    return await getAllLocations()
+    return await findAllLocations()
+}
+
+export async function deleteLocation(id: string): Promise<Location | null> {
+    if(!await getOneLocationByID(id)) {
+        throw new Error(`Lokalizacja o id ${id} nie istnieje, więc nie da się jej usunąć`)
+    } else {
+        return await deleteLocationByID(id)
+    }
 }
